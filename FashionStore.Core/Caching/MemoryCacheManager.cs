aaa67e5b80eb9705b4a699a6a85f6f9a385d2b34
wade712vn <wade712vn@gmail.com>
2014-02-20 16:52:07 +0700

    Added several files

---
+++ FashionStore.Core/Caching/MemoryCacheManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStore.Core.Caching
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        public ObjectCache Cache
+        {
+            get
+            {
+                return MemoryCache.Default;
+            }
+        }
+
+        public T Get<T>(string key) where T : class
+        {
+            return Cache[key] as T;
+        }
+    }
+}
